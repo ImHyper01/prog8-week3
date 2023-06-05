be942683f24391562ccf0dd1a12273919cf78e23
ImHyper01 <57798860+ImHyper01@users.noreply.github.com>
2023-06-05 20:21:29 +0200

    speach werkt

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-
-
-
 const video = document.getElementById("webcam");
 const label = document.getElementById("label");
 
@@ -8,12 +5,39 @@ const labelOneBtn = document.querySelector("#labelOne");
 const labelTwoBtn = document.querySelector("#labelTwo");
 const labelThreeBtn = document.querySelector("#labelThree");
 const trainbtn = document.querySelector("#train");
+let inputField = document.querySelector("#inputfield")
+let playButton = document.querySelector("#playbutton")
+
 
 labelOneBtn.addEventListener("click", () => console.log("button 1"));
 labelTwoBtn.addEventListener("click", () => console.log("button 2"));
 labelThreeBtn.addEventListener("click", () => console.log("button 3"));
 
 trainbtn.addEventListener("click", () => console.log("train"));
+
+
+let synth = window.speechSynthesis
+
+function speak(text) {
+    if (synth.speaking) {
+        console.log('still speaking...')
+        return
+    }
+    if (text !== '') {
+        let utterThis = new SpeechSynthesisUtterance(text)
+        synth.speak(utterThis)
+    }
+}
+
+playButton.addEventListener("click", () => {
+    let text = inputField.value
+    speak(text)
+})
+
+
+classifier.classify(document.getElementById('image'), (err, results) => {
+    console.log(results);
+});
 
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
